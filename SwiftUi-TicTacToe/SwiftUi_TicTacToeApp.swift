11d1525d1908cc7fb3496f435b1a3ca657bdec92
Anjola favour ayomikun <anjie167@gmail.com>
2023-02-05 16:21:40 +0100

    Initial Commit

---
+++ SwiftUi-TicTacToe/SwiftUi_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_TicTacToeApp.swift
+//  SwiftUi-TicTacToe
+//
+//  Created by FAVOUR ANJOLA on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
